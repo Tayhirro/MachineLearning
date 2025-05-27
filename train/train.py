c106559ab9faf423d6035c2ba9705d277098d09e
tayhirro <tayhirro@gmail.com>
2025-05-27 21:59:57 +0800

    cna

--- train/train.py
+++ train/train.py
@@ -1,22 +1,42 @@
 from ultralytics import YOLO
+import yaml
+import argparse
+import os
 
-# 1) 加载预训练模型
-model = YOLO("/home/jiahongyu/Detection/train/yolov8n.pt")
 
-# 2) 开始 finetune
-model.train(
-    data="/home/jiahongyu/Detection/datasets/meter/meter.yaml",  # 数据集配置文件
-    epochs=100,
-    imgsz=640,
-    batch=16,
-    device="0,1,2,3",  # 使用多卡训练
-    freeze=10           # 冻结前10层
-)
 
-# 3) 评估 test 集
-metrics = model.val(split="test")   # or split='val'
-print(metrics.box.map50)            # 举例：输出 mAP50
+def parse_args() -> argparse.Namespace:
+    args = argparse.ArgumentParser(description="yolo_Dect")
+    args.add_argument("--yaml_name",default="yoloDe.yaml")
+    return args.parse_args()
 
-# # 4) 预测一张新图
-# result = model("meter/images/test/your_img.jpg", conf=0.25)
-# result[0].show()                    # 可视化
+def main():
+    args = parse_args()
+    configs = os.path.join("../config",args.yaml_name)
+    with open(configs, "r") as f:
+        cfg = yaml.safe_load(f)
+
+    # 1) 加载预训练模型
+    model = YOLO(f"{cfg['base_path']}/{cfg['model_path']}")
+    # 2) 开始 finetune
+    model.train(
+        data=f"{cfg['base_path']}/{cfg['data_path']}",
+        epochs=cfg['epochs'],
+        imgsz=cfg['imgsz'],
+        batch=cfg['batch'],
+        device=cfg['device'],
+        freeze=cfg['freeze']
+    )
+    # 3) 评估 test 集
+    metrics = model.val(split="test")   # or split='val'
+    print(metrics.box.map50)            # 举例：输出 mAP50
+
+    # 5. 预测一张新图（可选）
+    # result = model(f"{cfg['base_path']}/meter/images/test/your_img.jpg", conf=0.25)
+    # result[0].show()                  # 可视化
+
+
+
+
+if __name__ == "__main__":
+    main()
